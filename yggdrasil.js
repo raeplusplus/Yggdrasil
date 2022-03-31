80a7a77bf5bae70da107333c51dd32c1f0046d22
Raeshelle Rose <raeshellerose@outlook.com>
2022-03-31 00:52:09 -0500

    added contact form and loader gif

--- yggdrasil.js
+++ yggdrasil.js
@@ -1,17 +1,19 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+carousel();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-//   var i;
+function carousel() {
+  var i;
   var x = document.getElementsByClassName("topSlideShow");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (let i = 0; i < x.length; i++) {
+  for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+// Loader Fade Out
+setTimeout(function () {
+  $('.loader_bg').fadeToggle();
+}, 1600);
