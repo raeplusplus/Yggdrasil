1cab9c20792171f271ecf1f3de0b7316e310c445
Raeshelle Rose <raeshellerose@outlook.com>
2022-04-20 20:38:42 -0500

    minor fixes - links, comments

--- yggdrasil.js
+++ yggdrasil.js
@@ -10,5 +10,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 2000); 
 }
